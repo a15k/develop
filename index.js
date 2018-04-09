4f5fbdab4772c9de3cb5664aec9ef27bcd3b5dd4
JP Slavinsky <jpslav@gmail.com>
2018-04-09 09:08:26 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import 'font-awesome/css/font-awesome.css';
+import './source/stylesheets/site.scss';
+import './source/stylesheets/swagger-ui.css';
+import './source/javascripts/site.js';
---
+++ source/javascripts/site.js
@@ -0,0 +1,16 @@
+require('script-loader!jquery');
+require('script-loader!tether');
+require('script-loader!bootstrap');
+// const SwaggerUI = require('swagger-ui');
+
+// window.SwaggerUI = SwaggerUI;
+
+window.SwaggerUI = require('swagger-ui');
+
+$(document).ready(function() {
+// SwaggerUI({
+//   dom_id: '#myDomId',
+//   url: "http://localhost:2901/api/docs/v1"
+// })
+
+});

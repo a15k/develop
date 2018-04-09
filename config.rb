38f63c78ebed076de5256bdc7756456c693b2c8b
JP Slavinsky <jpslav@gmail.com>
2018-04-09 13:14:24 -0600

    fixed s3 deployment

--- config.rb
+++ config.rb
@@ -7,3 +7,23 @@ page '/*.txt', layout: false
 activate :directory_indexes
 
 helpers WebpackAssetHelper
+
+raise "AWS credentials not set" if ENV['AWS_ACCESS_KEY_ID'].nil? || ENV['AWS_SECRET_ACCESS_KEY'].nil?
+
+activate :s3_sync do |s3_sync|
+  s3_sync.bucket                     = 'develop.a15k.org'
+  s3_sync.region                     = 'us-east-2'
+  s3_sync.aws_access_key_id          = ENV['AWS_ACCESS_KEY_ID']
+  s3_sync.aws_secret_access_key      = ENV['AWS_SECRET_ACCESS_KEY']
+  s3_sync.delete                     = false # We delete stray files by default.
+  s3_sync.after_build                = false # We do not chain after the build step by default.
+  s3_sync.prefer_gzip                = true
+  s3_sync.path_style                 = true
+  s3_sync.reduced_redundancy_storage = false
+  s3_sync.acl                        = 'public-read'
+  s3_sync.encryption                 = false
+  s3_sync.prefix                     = ''
+  s3_sync.version_bucket             = false
+  s3_sync.index_document             = 'index.html'
+  s3_sync.error_document             = '404.html'
+end

37ef0c5773e0d3db446a86d1a2326304fe90f625
JP Slavinsky <jpslav@gmail.com>
2018-07-04 15:36:42 -0600

    switched from swagger-ui npm module to CDN; couldn't get module to build properly

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import 'font-awesome/css/font-awesome.css';
 import './source/stylesheets/site.scss';
-import './source/stylesheets/swagger-ui.css';
 import './source/stylesheets/custom.scss';
 import './source/javascripts/site.js';
